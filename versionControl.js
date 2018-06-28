5265182a0dbd48eeaa1d9ca7459f694dde863f4e
PRElias <ppuspfc@gmail.com>
2018-06-28 09:26:53 -0300

    primeira versão do script recuperando a data do commit nos markdowns

---
+++ versionControl.js
@@ -0,0 +1,15 @@
+const exec = require('child_process').exec;
+
+const ls = exec('git log -1 --format=%cd input/index.md')
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
